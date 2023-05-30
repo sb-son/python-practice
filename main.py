428c29756cd6cbc01d49cd9908de6490e2c06a74
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-05-30 17:56:33 -0500

    function to convert seconds

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ if __name__ == '__main__':
 print('hello world')
 
 print(2+2)
+
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+hours, minutes, seconds = convert_seconds(5000)
+print(hours, minutes, seconds)
