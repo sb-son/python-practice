a02cae0308f8c0a5cd58bb9a8d35308197442b64
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-05-31 12:38:32 -0500

    following along with coursera. created lucky number function with random number generator

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import random
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -27,3 +29,11 @@ def convert_seconds(seconds):
 
 hours, minutes, seconds = convert_seconds(5000)
 print(hours, minutes, seconds)
+
+def lucky_number(name):
+    random_num = random.randint(1, 10)
+    number = len(name) * random_num
+    print("Hello " + name + ". Your lucky number is " + str(number) + ". Multiplier was " + str(random_num))
+
+lucky_number("Kay")
+lucky_number("Cameron")
