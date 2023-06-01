210a28bc975720b7e4c76398da65c78ac88e9dc8
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-01 11:55:10 -0500

    using function to calculate distance with python

--- main.py
+++ main.py
@@ -37,3 +37,63 @@ def lucky_number(name):
 
 lucky_number("Kay")
 lucky_number("Cameron")
+
+
+# This function calculates the number of days in a variable number of
+# years, months, and days. These variables are provided by the user and
+# are passed to the function through the function’s parameters.
+def find_total_days(years, months, days):
+    # Assign a variable to hold the calculations for the number of days in
+    # a year (years*365) plus the number of days in a month (months*30) plus
+    # the number of days provided through the "days" parameter variable.
+    my_days = (years * 365) + (months * 30) + days
+    # Use the "return" keyword to send the result of the "my_days"
+    # calculation to the function call.
+    return my_days
+
+
+# Function call with user provided parameter values.
+print(find_total_days(2, 5, 23))
+
+
+# This function converts fluid ounces to milliliters and returns the
+# result of the conversion.
+def convert_volume(fluid_ounce):
+    # Calculate value of the "ml" variable using the parameter variable
+    # "fluid_ounce". There are approximately 29.5 milliliters in 1 fluid
+    # ounce.
+    ml = fluid_ounce * 29.5
+    # Return the result of the calculation.
+    return ml
+
+
+# Call the conversion from within the print() function using 2 fluid
+# ounces. Convert the return value from a float to a string.
+print("The volume in millimeters is " + str(convert_volume(2)))
+
+# Call the function again and double the 2 fluid ounces from within
+# the print function.
+print("The volume in millimeters is " + str(convert_volume(2) * 2))
+# Alternative calculation:
+# print("The volume in millimeters is " + str(convert_volume(4))
+
+
+# 1) Complete the function to return the result of the conversion
+def convert_distance(miles):
+	km = miles * 1.6  # approximately 1.6 km in 1 mile
+	return km
+
+# Do not indent any of the following lines of code as they are
+# meant to be located outside of the function above
+
+my_trip_miles = 55
+
+# 2) Convert my_trip_miles to kilometers by calling the function above
+my_trip_km = convert_distance(my_trip_miles)
+
+# 3) Fill in the blank to print the result of the my_trip_km conversion
+print("The distance in kilometers is " + str(my_trip_km))
+
+# 4) Calculate the round-trip in kilometers by doubling the result of
+#    my_trip_km. Fill in the blank to print the result.
+print("The round-trip in kilometers is " + str(my_trip_km * 2))
