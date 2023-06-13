bae5f098a79ed37c382e5d32e7aae01cbd429449
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-13 12:58:13 -0500

    list comprehension practice

--- main.py
+++ main.py
@@ -279,3 +279,24 @@ for outer_loop in range(2, 6 + 1):
 
 weather = "Rainfall"
 print(weather[:4])
+
+multiples = [x*7 for x  in range(1, 11)]
+print(multiples)
+
+z = [x for x in range(0, 101) if x % 3 == 0]
+print(z)
+
+### Simple List Comprehension
+print("List comprehension result:")
+# The following list comprehension compacts several lines
+# of code into one line:
+print([x*2 for x in range(1,11)])
+
+### Long form for loop
+print("Long form code result:")
+# The list comprehension above accomplishes the same result as
+# the long form version of the code:
+my_list = []
+for x in range(1,11):
+   my_list.append(x*2)
+print(my_list)
