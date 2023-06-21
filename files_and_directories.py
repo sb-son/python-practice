c91c1a54efc5f9811a6bead7b4d6a43a6540acfa
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-21 11:23:06 -0500

    created function that creates a new python program

---
+++ files_and_directories.py
@@ -0,0 +1,12 @@
+import os.path
+
+
+def create_python_script(filename):
+  comments = "# Start of a new Python program"
+  with open(filename, 'w') as file:
+    file.write(comments)
+
+    filesize = os.path.getsize(filename)
+  return(filesize)
+
+print(create_python_script("program.py"))
