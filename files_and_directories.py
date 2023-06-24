e696f07b2d7e1dfed6c9b562729491b56197369a
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-24 18:32:36 -0500

    file date function creates a new file in current working directory, check date, and returns the date portion of the timestamp

--- files_and_directories.py
+++ files_and_directories.py
@@ -1,4 +1,6 @@
 import os.path
+import os
+import datetime
 
 
 def create_python_script(filename):
@@ -10,3 +12,17 @@ def create_python_script(filename):
   return(filesize)
 
 print(create_python_script("program.py"))
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open(filename, 'w') as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  # Convert the timestamp into a readable format, then into a string
+  date = datetime.datetime.fromtimestamp(timestamp).date()
+  # Return just the date portion
+  # Hint: how many characters are in “yyyy-mm-dd”?
+  return ("{:%Y-%m-%d}".format(date))
+
+print(file_date("newfile.txt"))
+# Should be today's date in the format of yyyy-mm-dd
